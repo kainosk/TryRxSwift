075895f91f2d91d3df824bb3a18daf81fb163d92
Kainosuke <kainosuke.obata@music.yamaha.com>
2020-04-22 12:00:23 +0900

    initial commit: setup RxSwift & XcodeGen

---
+++ TryRxSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryRxSwift
+//
+//  Created by kai on 2020/04/22.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
